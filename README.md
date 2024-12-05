b498f78624af8084b602487ffe69d23836108993
liangping.quan <liangping.quan@tuya.com>
2024-12-05 14:11:13 +0800

    btk20_src debug ok

--- README.md
+++ README.md
@@ -4,3 +4,24 @@ Open source C++ and Python libraries to facilitate research and development
 for spatial signal processing
 
 Please see http://distantspeechrecognition.sourceforge.net/ for project details.
+
+2024.12.05编译说明：
+
+环境必须是2.7，可以使用conda环境
+
+其次有几个依赖库，swig, sndfile, gsl是必备的。
+
+可以采用conda的形式安装sndfile, swig
+
+gsl库可以采用./configure --prefix=`pwd`/release; make; make install;的形式安装到指定目录；
+
+在最外侧的cmakelists.txt只需要注意几个路径即可
+
+在build执行最基本的cmake ..; make; make install;即可
+
+在python环境下执行import btk20; import btk20.feature as feat;如果正常则没有问题
+
+
+
+
+
---
+++ btk20_src/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=/home/hhquan/workspace/test_hhquan/test5/gsl_release/include:$C_INCLUDE_PATH      
+export CPLUS_INCLUDE_PATH=/home/hhquan/workspace/test_hhquan/test5/gsl_release/include:$CPLUS_INCLUDE_PATH        
+export LIBRARY_PATH=/home/hhquan/workspace/test_hhquan/test5/gsl_release/lib:$LIBRARY_PATH  
+export LD_LIBRARY_PATH=/home/hhquan/workspace/test_hhquan/test5/gsl_release/lib:$LD_LIBRARY_PATH  
+
+export C_INCLUDE_PATH=/home/hhquan/anaconda3/envs/tuya/include:$C_INCLUDE_PATH      
+export CPLUS_INCLUDE_PATH=/home/hhquan/anaconda3/envs/tuya/include:$CPLUS_INCLUDE_PATH        
+export LIBRARY_PATH=/home/hhquan/anaconda3/envs/tuya/lib:$LIBRARY_PATH  
+export LD_LIBRARY_PATH=/home/hhquan/anaconda3/envs/tuya/lib:$LD_LIBRARY_PATH  
